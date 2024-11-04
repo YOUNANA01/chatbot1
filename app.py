f949063b8de786b44efdbc1136ab0ed0be9698ed
YOUNANA01 <olivia72680105@gmail.com>
2024-11-04 20:38:54 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ load_dotenv()
  
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
  
-st.set_page_config(page_title="Nutrition Expert",page_icon="🗣️")
+st.set_page_config(page_title="Image Chat Bot",page_icon="🗣️")
  
-st.header("Nutrition Expert Web Application")
+st.header("My Image Chat Bot Web Application")
  
 question = st.text_input("Write your question here...")
  
@@ -23,22 +23,12 @@ submit = st.button("Submit")
 if uploaded_image is not None:
     image = Image.open(uploaded_image)
     st.image(image,caption="Uploaded Image",use_column_width=True)
-
-input_prompt = """You are a nutrition expert where you need to see the food items present
-in the input image and answer the question being asked , if the user don't ask any question
-then calculate the total calories, also provide the details of each food items with
-calorie intake in following format:
- 
-1. Item 1 - No of calories
-2. Item 2 - No of calories
--------
--------
- 
-Please don't answer any question which is not related to food or the uploaded image, simply
-say "I am not capable to answer this"
-"""
  
 if submit:
-    model = genai.GenerativeModel("gemini-1.5-pro")
-    response = model.generate_content([input_prompt,image,question])
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    if(input!=""):
+        response = model.generate_content([question,image])
+    else:
+        response = model.generate_content(image)
+ 
     st.write(response.text)
