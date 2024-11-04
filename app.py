99fdfeef3b136ff7cb4df82485507bed9ace09f0
YOUNANA01 <olivia72680105@gmail.com>
2024-11-04 20:40:24 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -8,9 +8,8 @@ load_dotenv()
  
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
  
-st.set_page_config(page_title="Image Chat Bot",page_icon="🗣️")
- 
-st.header("My Image Chat Bot Web Application")
+st.set_page_config(page_title="Invoice Expert",page_icon="🗣️")
+st.header("Invoice Extracter Web Application")
  
 question = st.text_input("Write your question here...")
  
@@ -24,11 +23,14 @@ if uploaded_image is not None:
     image = Image.open(uploaded_image)
     st.image(image,caption="Uploaded Image",use_column_width=True)
  
-if submit:
-    model = genai.GenerativeModel("gemini-1.5-flash")
-    if(input!=""):
-        response = model.generate_content([question,image])
-    else:
-        response = model.generate_content(image)
+input_prompt = """You are an expert in understanding invoices, you will get the input
+image as invoice and you will have to answer the question based on the input invoice.
+If you get any other image then invoice, then reply with the following message.
+```I understand invoices only, please upload a valid invoice and ask relevant questions
+only```
+"""
  
+if submit:
+    model = genai.GenerativeModel("gemini-1.5-pro")
+    response = model.generate_content([input_prompt,image,question])
     st.write(response.text)
